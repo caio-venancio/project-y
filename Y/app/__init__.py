a1ba0b4d2759de7dfa2979df9ba799b243c92289
caio-venancio <caio.venancio784@gmail.com>
2024-09-03 13:00:37 -0300

    Novo Y, começando do zero
    nosso amigo G não manjou das coisas e nós fomos muito inocentes também (muito complicado para gente).

--- Y/app/__init__.py
+++ Y/app/__init__.py
@@ -1,7 +1,7 @@
+print("Começando init")
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+
+#pegando o .env
 from dotenv import load_dotenv
 import os
 
@@ -11,10 +11,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
-
 from app import routes
+print("terminando init")
--- Y/app/routes.py
+++ Y/app/routes.py
@@ -1,67 +1,34 @@
+print('começando routes')
 from flask import render_template, url_for, flash, redirect, request
-from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm, PostForm
-from app.models import User, Post, Like
-from flask_login import login_user, current_user, logout_user, login_required
+from app import app
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
-@login_required
 def home():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Your post has been created!', 'success')
-        return redirect(url_for('home'))
-    
-    posts = Post.query.all()
-    return render_template('home.html', posts=posts, form=form)
+    return render_template('home.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=True)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html')
 
 @app.route("/logout")
 def logout():
-    logout_user()
     return redirect(url_for('login'))
 
 @app.route("/like/<int:post_id>", methods=['POST'])
-@login_required
 def like_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.likes.filter_by(user_id=current_user.id).first():
-        like = Like.query.filter_by(user_id=current_user.id, post_id=post_id).first()
-        db.session.delete(like)
-    else:
-        like = Like(user_id=current_user.id, post_id=post_id)
-        db.session.add(like)
-    db.session.commit()
+    # post = Post.query.get_or_404(post_id)
+    # if post.likes.filter_by(user_id=current_user.id).first():
+    #     like = Like.query.filter_by(user_id=current_user.id, post_id=post_id).first()
+    #     db.session.delete(like)
+    # else:
+    #     like = Like(user_id=current_user.id, post_id=post_id)
+    #     db.session.add(like)
+    # db.session.commit()
     return redirect(url_for('home'))
+
+print('terminando routes')
