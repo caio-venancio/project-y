add1610c80251533e97f3d69114bc8c7b0b673d4
caio-venancio <caio.venancio784@gmail.com>
2024-09-03 13:03:09 -0300

    Delete Y/config.py

--- Y/config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'site.db'))
