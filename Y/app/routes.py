7a7b4b5594678c892b0fd31bca17560b717736f1
caio-venancio <caio.venancio784@gmail.com>
2024-09-08 19:54:30 -0300

    Latest change, with login
    Eu alterei uma sequência de arquivos, e tem comentários e prints para debug. Utilize essa versão a partir de agora

---
+++ Y/app/routes.py
@@ -0,0 +1,36 @@
+print('começando routes')
+from flask import render_template, url_for, flash, redirect, request
+from app import app
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    #if se for post e tiver os argumentos certos, redirect to login
+    return render_template('register.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    #if se for post e tiver os argumentos certos, redirect to home
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for('login'))
+
+@app.route("/like/<int:post_id>", methods=['POST'])
+def like_post(post_id):
+    # post = Post.query.get_or_404(post_id)
+    # if post.likes.filter_by(user_id=current_user.id).first():
+    #     like = Like.query.filter_by(user_id=current_user.id, post_id=post_id).first()
+    #     db.session.delete(like)
+    # else:
+    #     like = Like(user_id=current_user.id, post_id=post_id)
+    #     db.session.add(like)
+    # db.session.commit()
+    return redirect(url_for('home'))
+
+print('terminando routes')
---
+++ Y/run.py
@@ -0,0 +1,5 @@
+from app import app
+
+if __name__ == '__main__':
+    print('começando APP')
+    app.run(debug=True)
